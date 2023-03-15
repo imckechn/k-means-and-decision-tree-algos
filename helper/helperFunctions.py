bd0a32d6122efbe5d3e77d6915df0cb766edae8e
Ian McKechnie <imckechn@uoguelph.ca>
2023-03-15 19:21:14 -0400

    Modualized code for next algo. Hard coded 3 nodes since that's in the spec

---
+++ helper/helperFunctions.py
@@ -0,0 +1,29 @@
+import random
+from kMeans.iris import Iris
+
+# This imports all the irises into an array of iris objects
+def parse_irises(fileLoc):
+    irises = []
+    try:
+        with open(fileLoc) as file:
+            lines = [line.rstrip() for line in file]
+
+    except:
+        print("File not found")
+        return []
+
+    for line in lines:
+        elements = line.split(",")
+        irises.append(Iris(elements[4], elements[0], elements[1], elements[2], elements[3]))
+
+    return irises
+
+
+#Split the data set into a training set and a test set, test_size needs to be a float for the percentage
+def train_test_split(irises, test_size):
+    random.shuffle(irises)
+    train_size = int(len(irises) * (1 - test_size))
+    train = irises[:train_size]
+    test = irises[train_size:]
+
+    return train, test
--- kmeans.py
+++ kmeans.py
@@ -1,48 +1,12 @@
-from kMeans.iris import Iris
 from kMeans.centroid import Centroid
 import math
 import random
-
-# This imports all the irises into an array of iris objects
-def parse_irises(fileLoc):
-    irises = []
-    try:
-        with open(fileLoc) as file:
-            lines = [line.rstrip() for line in file]
-
-    except:
-        print("File not found")
-        return []
-
-    for line in lines:
-        elements = line.split(",")
-        irises.append(Iris(elements[4], elements[0], elements[1], elements[2], elements[3]))
-
-    return irises
-
-
-#Split the data set into a training set and a test set, test_size needs to be a float for the percentage
-def train_test_split(irises, test_size):
-    random.shuffle(irises)
-    train_size = int(len(irises) * (1 - test_size))
-    train = irises[:train_size]
-    test = irises[train_size:]
-
-    return train, test
-
+from helper.helperFunctions import *
 
 #This creates all the centroids and returns a list of centroid objects
 def create_centroids():
-    while True:
-        #count = input("How many centroids would you like to create? ")
-        count = "3"
-
-        if count.isdigit():
-            break
-
-    count = int(count)
     centroids = []
-    for i in range(count):
+    for i in range(3):
         centroids.append(Centroid())
 
     return centroids
